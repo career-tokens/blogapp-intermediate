35b34f9ec36872f19b81df82ed085c056dea62dc
mainak <heellomeelo@gmail.com>
2023-04-11 21:27:37 +0530

    final commit

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Bloglist from './Bloglist';
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'topic1', body: 'body1', author: 'author1', id: 1 },
-        { title: 'topic2', body: 'body2', author: 'author2', id: 2 },
-        { title: 'topic3', body: 'body3', author: 'author3', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
   const handleDelete = (id) => {
     setBlogs(blogs.filter((blog) => blog.id !== id));
   }
+
+  useEffect(() => {
+    fetch('http://localhost:3000/blogs')
+      .then(res => {
+        return res.json();
+      }).then(data => {
+        console.log(data);
+      })
+  }, []);
   return (
     <>
-      <Bloglist blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />
+     {/**  <Bloglist blogs={blogs} title={"All Blogs"} handleDelete={handleDelete} />*/}
       {/**<Bloglist blogs={blogs.filter((blog) => blog.author == 'author1')} title={"Author1's Blogs"} />*/}
     </>
   )
